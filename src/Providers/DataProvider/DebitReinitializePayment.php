2bce3defe62a51a3c760f2fe34b55430dca0ec10
Marcel Pönninghaus <>
2019-04-04 19:28:12 +0200

    additional check for existing payments (myAccount and orderConfirmation)

--- src/Providers/DataProvider/DebitReinitializePayment.php
+++ src/Providers/DataProvider/DebitReinitializePayment.php
@@ -2,6 +2,7 @@
 
 namespace Debit\Providers\DataProvider;
 
+use Plenty\Modules\Payment\Contracts\PaymentRepositoryContract;
 use Plenty\Plugin\Templates\Twig;
 use Debit\Helper\DebitHelper;
 
@@ -12,6 +13,18 @@ class DebitReinitializePayment
         /** @var DebitHelper $paymentHelper */
         $paymentHelper = pluginApp(DebitHelper::class);
         $paymentId = $paymentHelper->getDebitMopId();
-        return $twig->render('Debit::DebitReinitializePayment', ["order" => $arg[0], "paymentMethodId" => $paymentId]);
+
+        $order = $arg[0];
+
+        /** @var PaymentRepositoryContract $paymentRepo */
+        $paymentRepo = pluginApp(PaymentRepositoryContract::class);
+        $orderPayments = $paymentRepo->getPaymentsByOrderId($order['id']);
+
+        $hasNoPayments = false;
+        if ($orderPayments == null) {
+            $hasNoPayments = true;
+        }
+
+        return $twig->render('Debit::DebitReinitializePayment', ["order" => $order, "paymentMethodId" => $paymentId, "paymentExists" => !$hasNoPayments]);
     }
 }
